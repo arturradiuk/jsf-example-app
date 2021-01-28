2ea664c64f69b12b265eaff6207475ec73fbf7d3
arturradiuk <arturradiuk@gmail.com>
2021-01-28 20:46:13 +0200

    javascript support has been added

---
+++ src/main/webapp/resources/js/common.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    alert("hello world dear friend")
+    // fetch("http://example.com")
+    // location.replace("http://localhost:8080/pas/faces/NewPerson.xhtml")
+    window.location.href = "http://localhost:8080/pas/faces/NewPerson.xhtml";
+}
